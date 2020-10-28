7a40983d9bb2bd2d9710d0ee74fc2e7ba5448a36
David Ganz <david.g.ganz@gmail.com>
2020-10-28 17:35:37 +0100

    Update Swap delay to 5 min

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -3,6 +3,6 @@ package util;
 public class Constants {
 
     public static final int INVITE_EXPIRE_DElAY = 60 * 20;                                                  // -> sec * ticks
-    public static final int SWAP_PLAYERS_DELAY = ((60 * 1) - 10) * 20;                                      // -> ((sec(per min) * min(Anz)) - 10 (-> 10 sec earlier because 10 sec countdown)) * ticks
+    public static final int SWAP_PLAYERS_DELAY = ((60 * 5) - 10) * 20;                                      // -> ((sec(per min) * min(Anz)) - 10 (-> 10 sec earlier because 10 sec countdown)) * ticks
 
 }
